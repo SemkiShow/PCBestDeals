498b452cb75b098bae4a40fc3a48eef2d0d9d71b
SemkiShow <mgdeveloper123@gmail.com>
2025-09-15 20:36:14 +0200

    Fix release.sh

--- release.sh
+++ release.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 set -e
+executable_name=PCBestDeals
 
 # Compiling for Linux
 cmake -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j
-cp build/bin/asciify .
+cp build/bin/$executable_name .
 
 # Compiling for Windows
 cmake -B build_win -DCMAKE_TOOLCHAIN_FILE=mingw-w64-x86_64.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build build_win -j
-cp build_win/bin/asciify.exe .
+cp build_win/bin/$executable_name.exe .
 
 # Zipping the build
-zip release.zip asciify asciify.exe ffmpeg ffmpeg.exe LICENSE README.md
-rm asciify asciify.exe
+zip release.zip $executable_name $executable_name.exe LICENSE README.md
+rm $executable_name $executable_name.exe
 
 # Creating a GitHub release
 gh release create $1 release.zip
